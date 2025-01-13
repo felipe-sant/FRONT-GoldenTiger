b3dedc71e4c33274d3d37daff2d1bf4b272a1268
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2025-01-13 14:40:09 +0000

    feat: adding viewPassword function #2

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -1,6 +1,11 @@
+import React from "react"
 import css from "../styles/pages/login.module.css"
 
 export default function LoginPage() {
+    const [username, setUsername] = React.useState<string>("")
+    const [password, setPassword] = React.useState<string>("")
+    const [viewPassword, setViewPassowrd] = React.useState<boolean>(false)
+
     return (
         <main className={css.main}>
             <div className={css.wallpaper}>
@@ -20,13 +25,19 @@ export default function LoginPage() {
                 <div className={css.form}>
                     <div className={css.field}>
                         <label htmlFor="username">Nome de Usuário</label>
-                        <input type="text" />
+                        <input 
+                            type="text" 
+                        />
                     </div>
                     <div className={css.field}>
                         <label htmlFor="username">Senha</label>
-                        <input type="password" className={css.password} />
-                        <button className={css.viewPassword}>
-                            <img src="" alt=" " />
+                        <input
+                            id="password" 
+                            type={viewPassword ? "text" : "password"}
+                            className={css.password}
+                        />
+                        <button className={css.viewPassword} onClick={() => setViewPassowrd(!viewPassword)}>
+                            <img src="" alt=" "/>
                         </button>
                         <a href="">Esqueceu a senha?</a>
                     </div>
