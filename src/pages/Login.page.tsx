69c4cb40c9b19d762c88a8b8cda90964d7b70f4a
Luiz Felipe <felipe.pinghang2@gmail.com>
2025-01-15 15:50:45 -0300

    style: adding colors to inputs with errors and logging signaling #2

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -7,37 +7,73 @@ import usernameMask from "../functions/utils/usernameMask"
 import ThemeContext from "../context/Theme.context"
 import UserConnection from "../backend/routes/UserConnection"
 import LoggedUserContext from "../context/LoggedUser.context"
+import loading from "../assets/images/loading.svg"
+import inputFocus from "../functions/utils/inputFocus"
 
 export default function LoginPage() {
     const { theme } = React.useContext(ThemeContext);
     const { updateToken } = React.useContext(LoggedUserContext)
+    const usernameRef = React.useRef<HTMLInputElement>(null)
     const [username, setUsername] = React.useState<string>("")
+    const [errorUsername, setErrorUsername] = React.useState<boolean>(false)
+    const passwordRef = React.useRef<HTMLInputElement>(null)
     const [password, setPassword] = React.useState<string>("")
+    const [errorPassword, setErrorPassword] = React.useState<boolean>(false)
     const [viewPassword, setViewPassword] = React.useState<boolean>(false)
     const [errorMessage, setErrorMessage] = React.useState<string>()
+    const [isLogin, setIsLogin] = React.useState<boolean>(false)
+
+    function clearErrors() {
+        setErrorUsername(false)
+        setErrorPassword(false)
+        setErrorMessage("")
+    }
+
+    function enableErrorUsername() {
+        setErrorUsername(true)
+        inputFocus(usernameRef)
+    }
+
+    function enableErrorPassword() {
+        setErrorPassword(true)
+        inputFocus(passwordRef)
+    }
 
     async function login() {
+        if (isLogin) return
+        setIsLogin(true)
+        clearErrors()
+
         if (username === "") {
             setErrorMessage("Username is required")
+            enableErrorUsername()
+            setIsLogin(false)
             return
         }
+
         if (password === "") {
             setErrorMessage("Password is required")
+            enableErrorPassword()
+            setIsLogin(false)
             return
         }
+
         const response = await UserConnection.loginUser(username, password) as any
         if (response.token) {
             updateToken(response.token)
             window.location.href = "/"
         } else {
             setErrorMessage(response.message)
+            if (response.message === "User not found") enableErrorUsername()
+            if (response.message === "Password is incorrect") enableErrorPassword()
+            setIsLogin(false)
         }
     }
 
     return (
         <main className={css.main}>
             <div className={css.wallpaper} />
-            <div className={css.tab  + " " + (theme === "dark" ? css.dark : css.light)}>
+            <div className={css.tab + " " + (theme === "dark" ? css.dark : css.light)}>
                 <div className={css.logo}>
                     <div className={css.img} />
                     <div className={css.text}>
@@ -48,21 +84,26 @@ export default function LoginPage() {
                 </div>
                 <div className={css.form}>
                     <div className={css.field}>
-                        <label htmlFor="username">Nome de Usuário</label>
+                        <label>Nome de Usuário</label>
                         <input
                             type="text"
                             value={username}
+                            className={errorUsername ? css.error : ""}
                             onChange={(e) => usernameMask(e.target.value, setUsername)}
+                            ref={usernameRef}
+                            onClick={clearErrors}
                         />
                     </div>
                     <div className={css.field}>
-                        <label htmlFor="username">Senha</label>
+                        <label>Senha</label>
                         <input
                             id="password"
                             type={viewPassword ? "text" : "password"}
-                            className={css.password}
+                            className={errorPassword ? css.password + " " + css.error : css.password}
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
+                            ref={passwordRef}
+                            onClick={clearErrors}
                         />
                         <button className={css.viewPassword} onClick={() => changeViewPassword(viewPassword, setViewPassword)}>
                             {viewPassword ? <img src={unView} alt="" /> : <img src={view} alt="" />}
@@ -74,7 +115,9 @@ export default function LoginPage() {
                     </div>
                     <div className={css.buttons}>
                         <a className={css.register} href="/register">CADASTRAR</a>
-                        <button className={css.login} type="submit" onClick={login}>ENTRAR</button>
+                        <button className={css.login} type="submit" onClick={login}>
+                            {isLogin ? <img src={loading} alt="" className={css.rotate} /> : <>ENTRAR</>}
+                        </button>
                     </div>
                 </div>
             </div>
